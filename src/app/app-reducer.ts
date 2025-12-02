ed8a580e5bef49c526159378bca62d637c2f3c7a
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-12-02 12:12:07 +0300

    add appSlice in app-reducer

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,16 +1,30 @@
-import { createAction, createReducer } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit"
 
-const initialState = {
-  themeMode: "light" as ThemeMode,
-}
-
-export const changeThemeModeAC = createAction<{ themeMode: ThemeMode }>("app/changeThemeMode")
-
-export const appReducer = createReducer(initialState, (builder) => {
-  builder.addCase(changeThemeModeAC, (state, action) => {
-    // логика мутабельного изменения стейта при изменении темы
-    state.themeMode = action.payload.themeMode
-  })
+const appSlice = createSlice({
+  name: "app",
+  initialState: {
+    themeMode: "light" as ThemeMode,
+  },
+  reducers: (create) => {
+    return {
+      // 1. reducers -  2. action creators
+      changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
+        state.themeMode = action.payload.themeMode
+      }),
+    }
+  },
 })
+
+export const appReducer = appSlice.reducer
+export const { changeThemeModeAC } = appSlice.actions
+
+// export const changeThemeModeAC = createAction<{ themeMode: ThemeMode }>("app/changeThemeMode")
+//
+// export const appReducer = createReducer(initialState, (builder) => {
+//   builder.addCase(changeThemeModeAC, (state, action) => {
+//     // логика мутабельного изменения стейта при изменении темы
+//     state.themeMode = action.payload.themeMode
+//   })
+// })
 
 export type ThemeMode = "dark" | "light"
