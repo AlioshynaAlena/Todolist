8177b35ec777a38bb6e3d8e36593ef0239a8adc8
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-12-31 16:53:04 +0300

    add useForm

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,15 +1,18 @@
 import { Main } from "@/app/Main"
 import { Login } from "@/features/todolists/ui/Login/Login"
 import { Route, Routes } from "react-router"
+import { PageNotFound } from "@/common/components/PageNotFound/PageNotFound.tsx"
 
 export const Path = {
   Main: "/",
   Login: "/login",
+  NotFoud: "*",
 } as const
 
 export const Routing = () => (
   <Routes>
     <Route path={Path.Main} element={<Main />} />
     <Route path={Path.Login} element={<Login />} />
+    <Route path={Path.NotFoud} element={<PageNotFound />} />
   </Routes>
 )
--- src/features/todolists/ui/Login/Login.tsx
+++ src/features/todolists/ui/Login/Login.tsx
@@ -9,11 +9,27 @@ import FormControlLabel from "@mui/material/FormControlLabel"
 import FormGroup from "@mui/material/FormGroup"
 import FormLabel from "@mui/material/FormLabel"
 import TextField from "@mui/material/TextField"
+import { useForm } from "react-hook-form"
+import styles from "./Login.module.css"
 
 export const Login = () => {
   const themeMode = useAppSelector(selectThemeMode)
 
   const theme = getTheme(themeMode)
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<LoginInputs>({ defaultValues: { email: "", password: "", rememberMe: false } })
+
+  const onSumbit = (data: LoginInputs) => {
+    console.log(data)
+    //Для очистки формы после успешного выполнения onSubmit
+    reset()
+  }
 
   return (
     <Grid container justifyContent={"center"}>
@@ -38,15 +54,35 @@ export const Login = () => {
             <b>Password:</b> free
           </p>
         </FormLabel>
-        <FormGroup>
-          <TextField label="Email" margin="normal" />
-          <TextField type="password" label="Password" margin="normal" />
-          <FormControlLabel label="Remember me" control={<Checkbox />} />
-          <Button type="submit" variant="contained" color="primary">
-            Login
-          </Button>
-        </FormGroup>
+        <form onSubmit={handleSubmit(onSumbit)}>
+          <FormGroup>
+            <TextField
+              label="Email"
+              margin="normal"
+              error={!!errors.email}
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Incorrect email address",
+                },
+              })}
+            />
+            {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
+            <TextField type="password" label="Password" margin="normal" {...register("password")} />
+            <FormControlLabel label="Remember me" control={<Checkbox />} {...register("rememberMe")} />
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
+          </FormGroup>
+        </form>
       </FormControl>
     </Grid>
   )
+}
+
+type LoginInputs = {
+  email: string
+  password: string
+  rememberMe: boolean
 }
