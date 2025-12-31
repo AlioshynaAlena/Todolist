c1690417ce4d1136e95731cd9fbe7eae26b13a91
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-12-31 17:50:29 +0300

    add button to the PageNotFound for redirection to the Main page

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,14 @@
+import { Button, Container } from "@mui/material"
 import styles from "./PageNotFound.module.css"
+import { Path } from "@/common/routing/Routing"
+import { Link } from "react-router"
 
 export const PageNotFound = () => (
-  <>
+  <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
-  </>
+    <Button variant="contained" component={Link} to={Path.Main} sx={{ width: "330px", mt: "20px" }}>
+      Вернуться на главную
+    </Button>
+  </Container>
 )
