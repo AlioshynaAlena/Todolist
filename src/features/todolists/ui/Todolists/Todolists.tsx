460cc87de1f8030bffe8d9d8ffcec68994573ad6
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-01-07 21:24:23 +0300

    delete Skip in Todolists

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,21 +1,12 @@
 import { Grid, Paper } from "@mui/material"
 import { TodolistItem } from "@/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx"
 import { useGetTodolistsQuery } from "@/features/todolists/api/_todolistsApi.ts"
-import { useState } from "react"
 
 export const Todolists = () => {
-  const [skip, setSkip] = useState(true)
-  const { data: todolists } = useGetTodolistsQuery(undefined, { skip })
-
-  const fetchTodolists = () => {
-    setSkip(false)
-  }
+  const { data: todolists } = useGetTodolistsQuery()
 
   return (
     <>
-      <div>
-        <button onClick={fetchTodolists}>Download todolists</button>
-      </div>
       {todolists?.map((todolist) => {
         return (
           <Grid key={todolist.id}>
