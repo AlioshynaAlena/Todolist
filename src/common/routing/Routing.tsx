99f4b689c60e0f23ec5da3a5c3b34ee6b374631c
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-01-25 20:04:59 +0300

    routing change

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -7,7 +7,7 @@ import { useAppSelector } from "@/common/hooks/useAppSelector.ts"
 import { selectIsLoggedIn } from "@/app/app-slice.ts"
 
 export const Path = {
-  Main: "/",
+  Main: "/Todolist",
   Login: "/login",
   Faq: "/faq",
   NotFound: "*",
