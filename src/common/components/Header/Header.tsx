4bf859fba82c66d85f93eb489bd2de2272a9f1c1
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-01-25 22:17:15 +0300

    Faq

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -5,7 +5,6 @@ import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
 import { getTheme } from "@/common/theme/theme.ts"
 import { changeThemeModeAC, selectIsLoggedIn, selectStatus, selectThemeMode, setIsLoggedInAC } from "@/app/app-slice.ts"
 import { containerSx } from "@/common/styles/container.styles.ts"
-
 import { useLogoutMutation } from "@/features/auth/api/authApi.ts"
 import { ResultCode } from "@/common/enums/enums.ts"
 import { AUTH_TOKEN } from "@/common/constants"
@@ -44,9 +43,7 @@ export const Header = () => {
         <Container maxWidth={"lg"} sx={containerSx}>
           <IconButton color="inherit"></IconButton>
           <Box display="flex" alignItems="center" gap={2} mr={2}>
-            {/*{user && <Typography variant="body1">Hello, {user.login}</Typography>}*/}
             {isLoggedIn && <NavButton onClick={logoutHandler}>Sign out</NavButton>}
-            <NavButton background={theme.palette.primary.dark}>Faq</NavButton>
             <Switch color={"default"} onChange={changeMode} />
           </Box>
         </Container>
